a009b8d194cf0462c6cd7036dc3d324348f53c31
dotbot56 <121630009+dotbot56@users.noreply.github.com>
2024-05-31 20:31:03 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("images");
     return {
         dir: {
             input: ".",
