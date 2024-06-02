89790bf4c58663b6e55418ff5d7f42a18914643c
dotbot56 <121630009+dotbot56@users.noreply.github.com>
2024-06-02 19:23:32 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("READNE.md");
+    eleventyConfig.addPassthroughCopy("README.md");
     return {
         dir: {
             input: ".",
